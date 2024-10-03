56a581aa89587d7088d4298ccbece3147e9789e4
Alex Lee <islicy@outlook.com>
2024-10-03 16:37:54 -0400

    Initial commit for 1.12.2

---
+++ minecraft_continuum_1.12.2_mod/src/main/java/mod/crabmods/minecraft_continuum/MinecraftContinuum.java
@@ -0,0 +1,31 @@
+package mod.crabmods.minecraft_continuum;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+    modid = MinecraftContinuum.MODID,
+    name = MinecraftContinuum.NAME,
+    version = MinecraftContinuum.VERSION)
+public class MinecraftContinuum {
+  public static final String MODID = "minecraft_continuum";
+  public static final String NAME = "Minecraft Continuum";
+  public static final String VERSION = "1.0.2";
+
+  private static Logger logger;
+
+  @EventHandler
+  public void preInit(FMLPreInitializationEvent event) {
+    logger = event.getModLog();
+  }
+
+  @EventHandler
+  public void init(FMLInitializationEvent event) {
+    // some crabmods code
+    logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+  }
+}
