a367502abcd6224a20ddc7e6855290d1f95d8c4d
Alex Lee <islicy@outlook.com>
2024-10-03 20:04:00 -0400

    Fix models for 1.12.2

--- minecraft_continuum_1.12.2_mod/src/main/java/mod/crabmods/minecraft_continuum/MinecraftContinuum.java
+++ minecraft_continuum_1.12.2_mod/src/main/java/mod/crabmods/minecraft_continuum/MinecraftContinuum.java
@@ -1,12 +1,18 @@
 package mod.crabmods.minecraft_continuum;
 
+import java.util.Objects;
 import mod.crabmods.minecraft_continuum.config.ConfigHandler;
 import mod.crabmods.minecraft_continuum.registers.ItemRegister;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.Logger;
 
 @Mod(
@@ -25,11 +31,26 @@ public class MinecraftContinuum {
     logger = event.getModLog();
     ConfigHandler.init(event);
     ItemRegister.registerItems();
+    // Only register item models on the client side
+    if (event.getSide().isClient()) {
+      for (Item item : ItemRegister.WORLD_CRYSTALS) {
+        registerItemModel(item);
+      }
+    }
+
   }
 
   @EventHandler
   public void init(FMLInitializationEvent event) {
     // some crabmods code
     logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+  }
+
+  @SideOnly(Side.CLIENT)
+  private void registerItemModel(Item item) {
+    ModelLoader.setCustomModelResourceLocation(
+        item,
+        0,
+        new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory"));
   }
 }
